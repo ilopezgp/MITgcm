cb4f7ff3c603419e291450366338d364a47f5b71
bderembl <bruno.deremble@ens.fr>
2018-08-02 16:54:50 -0400

    PE diag: wb with w = 0.5*(w(t+1) + w(t))

--- pkg/diagnostics/DIAGNOSTICS_EVAR.h
+++ pkg/diagnostics/DIAGNOSTICS_EVAR.h
@@ -15,7 +15,12 @@ C --- energy diagnostics
       _RL v_dphdy(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
       _RL v_ab   (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
 
+      _RL rho_sav(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+      LOGICAL witer0
+
       COMMON / DIAG_STORE_EPACKU / u_dissh, u_dissv, 
      &   u_advec, u_cori, u_ext, u_dphdx, u_ab
       COMMON / DIAG_STORE_EPACKV / v_dissh, v_dissv, 
      &   v_advec, v_cori, v_ext, v_dphdy, v_ab
+      COMMON / DIAG_STORE_EPACKRHO / rho_sav 
+      COMMON / DIAG_STORE_EPACKLOG / witer0
